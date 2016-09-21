2d03beb020a89ea308171dd5db193e08f18d89a0
Mo BETTA <Mo@MBMacBookPro.local>
2016-09-21 11:50:23 -0700

    Removed 'f' from condition

--- src/spec/capitalizeSpec.js
+++ src/spec/capitalizeSpec.js
@@ -6,7 +6,7 @@ const capitalize = flamingo.capitalize
 describe("capitalize", () => {
   let str = "string"
 
-  fit("Capitalize the first letter of a string to uppercase and all else to lowercase", () => {
+  it("Capitalize the first letter of a string to uppercase and all else to lowercase", () => {
     expect(capitalize(str)).toEqual("String")
   })
 
