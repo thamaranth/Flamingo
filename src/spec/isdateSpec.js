63242e76c7a5d1e897606b5a676b55e8084dde27
Debrena McEwen <debrenamcewen@Debrenas-MacBook-Pro.local>
2016-09-21 12:32:23 -0700

    Created isDate function

---
+++ src/spec/isdateSpec.js
@@ -0,0 +1,18 @@
+import { default as flamingo } from '../flamingo'
+
+const isDate = flamingo.isdate
+
+describe( "isdate", ()=> {
+  it('Checks if value is classified as a Date object.', () => {
+    expect( isDate( 'Mon April 23 2012' ) ).toEqual( false )
+  })
+
+  it('Checks if value is classified as a Date object.', () => {
+    expect( isDate( new Date ) ).toEqual( true )
+  })
+
+  it('Checks if value is classified as a Date object.', () => {
+    expect( isDate( {} ) ).toEqual( false )
+  })
+
+})
