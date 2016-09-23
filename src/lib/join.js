620a6c4dfffca45ae7e81be0cb6ad30151eef487
Devon Wesley <dwesley89@gmail.com>
2016-09-23 10:56:33 -0700

    join function refactored

--- src/lib/join.js
+++ src/lib/join.js
@@ -1,35 +1,11 @@
 export default (arr, sep) => {
-  let newArr = []
-  for (let i = 0; i < arr.length; ++i) {
-    newArr.push(arr[i])
-    if (i < (arr.length - 1)) {
-      newArr.push(sep)
+  let result = ''
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i + 1] == undefined){
+      result = result + arr[i]
+      break
     }
+    result = result + arr[i] + sep
   }
-
-  let joinC = newArr + ''
-
-  const trim = (str) => {
-    let char = ','
-
-    let output = ''
-    for( let i in str ){
-      let check = 0
-
-      for( let j in char ){
-        if (str[ i ] !== char[ j ]) {
-
-          check++
-          if( check === char.length ) {
-
-            output += str[i]
-          }
-        }
-      }
-    }
-
-    return output
-  }
-
-  return trim( joinC )
+  return result
 }
