63af9062e2f21c8a3bf1c35fd1769bcd8f7c4506
thamaranth <thamaranth@gmail.com>
2016-09-22 17:51:28 -0700

    easy Boolean check

---
+++ src/lib/isBoolean.js
@@ -0,0 +1,7 @@
+export default (bool) => {
+
+  var test = true;
+  if (typeof test === 'boolean')
+  console.log ("hmmm",test)
+     return test
+}
---
+++ src/spec/isBooleanSpec.js
@@ -0,0 +1,15 @@
+import { default as flamingo } from '../flamingo'
+import _ from 'lodash'
+
+const isBoolean = flamingo.isBoolean
+
+describe( 'isBoolean', () => {
+  it('Checks if value is classified as a boolean primitive or object.', () => {
+    expect( isBoolean(7 > 9) ).toEqual(true)
+  })
+})
+//  describe( 'isBoolean', () => {
+  //  it('Checks if value is classified as a boolean primitive or object.', () => {
+    //  expect( isBoolean('words') ).toEqual(false)
+  //  })
+//})
