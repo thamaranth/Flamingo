60d752c48155c07dfb8df5282a148598e05f23f5
Salman  Salman <salmansalman@Salmans-MacBook-Pro.local>
2016-09-23 11:52:22 -0700

    uploading the compact method

---
+++ src/spec/compactSpec.js
@@ -0,0 +1,10 @@
+import {default as flamingo} from "../flamingo"
+import _ from "lodash"
+
+const compact  = flamingo.compact
+
+fdescribe("compact", () => {
+  it("Creates an array with all falsey values removed. The values false, null, 0, \"\", undefined, and NaN are falsey", () => {
+    expect( compact([0, 1, false, 2, "", 3]) ).toEqual([1, 2, 3])
+  })
+})
